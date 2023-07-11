6f33e485cc393881f5dfaaf6f91d1ca8c63ed0d5
Ashu912 <ashutosh.19cs327@citranchi.ac.in>
2023-07-11 17:56:07 +0530

    final Commit

---
+++ src/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './Components/Screen/Home/Index';
+
+
+function App() {
+  return (
+    <div>
+      <Home />
+     
+    </div>
+  );
+}
+
+export default App;
